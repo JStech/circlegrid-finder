4150b9f7537de751cb68700377b2c8511124f3ca
John Stechschulte <john.l.stechschulte@gmail.com>
2024-02-21 16:17:03 -0500

    working!

--- ransac_homography.py
+++ ransac_homography.py
@@ -5,6 +5,7 @@ from matplotlib import colormaps
 import cv2 as cv
 from itertools import product, combinations
 from math import sin, cos, tan, pi, log
+from collections import defaultdict
 
 def gen_grid_pts(symmetric, grid_size):
     """Makes grid according to specifications, centered at the origin with largest dimension approximately filling [-1, 1]"""
@@ -111,51 +112,81 @@ def find_grid_points(pts, seed, grid_size, symmetric):
     """Identify which points in pts are near integer coordinates"""
     # round
     pts_rounded = np.rint(pts)
+    bbox = [np.min(pts_rounded[:, :2], axis=0), np.max(pts_rounded[:, :2])]
+
+    # reject outliers
+    outliers = set()
+    for i, j in combinations(range(pts.shape[0]), 2):
+        if np.linalg.norm(pts_rounded[i, :] - pts_rounded[j, :]) < 1e-6:
+            int_dist_i = np.linalg.norm(pts_rounded[i, :] - pts[i, :])
+            int_dist_j = np.linalg.norm(pts_rounded[j, :] - pts[j, :])
+            outliers.add(i if int_dist_i > int_dist_j else j)
+
+    for i in range(pts.shape[0]):
+        neighbors = 0
+        for v in (np.array([1, 0, 0]), np.array([-1, 0, 0]), np.array([0, 1, 0]), np.array([0, -1, 0])):
+            neighbors += (np.min(np.linalg.norm(pts_rounded - (pts_rounded[i, :] + v), axis=1)) < 1e-6)
+        if neighbors == 0:
+            outliers.add(i)
+
+    inliers = set(range(pts.shape[0])) - outliers
+    if len(inliers) < grid_size[0] * grid_size[1]:
+        return
+
+    # lines that might align with grid axes
+    axis_lines = [
+            np.array([ 0,  1,  0]),
+            np.array([ 1,  0,  0]),
+            np.array([ 1,  1,  0]),
+            np.array([ 1, -1,  0]),
+            np.array([ 1,  2,  0]),
+            np.array([ 1, -2,  0]),
+            np.array([ 2,  1,  0]),
+            np.array([ 2, -1,  0]),
+            ]
+
+    residual_distributions = []
+    x_axis_limits = []
+    y_axis_limits = []
+    for axis_line in axis_lines:
+        distribution = defaultdict(int)
+        for pt in pts_rounded[list(inliers), :]:
+            distribution[int(np.dot(axis_line, pt))] += 1
+        residual_distributions.append(distribution)
+
+        # if this in fact is the x axis, which lines (residuals) have the correct number of points?
+        x_enough_pts = [r for r, c in distribution.items() if c >= grid_size[0] // (2 - symmetric)]
+        if len(x_enough_pts) >= grid_size[1] * (2 - symmetric) and 0 in x_enough_pts:
+            x_axis_limits.append((min(x_enough_pts), max(x_enough_pts)))
+        else:
+            x_axis_limits.append(None)
+        y_enough_pts = [r for r, c in distribution.items() if c >= grid_size[1]]
+        if len(y_enough_pts) >= grid_size[0] and 0 in y_enough_pts:
+            y_axis_limits.append((min(y_enough_pts), max(y_enough_pts)))
+        else:
+            y_axis_limits.append(None)
 
     grid_ids = -1 * np.ones(grid_size, dtype=int)
-    # backtracking search to fill in grid IDs
-    position = 0
-    while np.sum(grid_ids == -1) > 0:
-        print("position", position)
-        idx = np.unravel_index(position, grid_size)
-        for i in range(grid_ids[idx]+1, pts.shape[0]):
-            if i >= 0 and i in grid_ids.reshape((-1))[:position]:
-                continue
-            grid_ids[np.unravel_index(position, grid_size)] = i
-            if check_grid(pts_rounded, grid_ids, grid_size, symmetric):
-                position += 2
-                break
-        if position == grid_size[0] * grid_size[1] and check_grid(pts_rounded, grid_ids, grid_size, symmetric):
-            return grid_ids
-        grid_ids[np.unravel_index(position, grid_size)] = -1
-        position -= 1
-    return None
 
+    # usually just one pair of axes meets the above criteria
+    for i_x, i_y in product(range(len(axis_lines)), repeat=2):
+        if i_x == i_y or x_axis_limits[i_x] is None or y_axis_limits[i_y] is None: continue
 
+        x_axis = axis_lines[i_x]
+        y_axis = axis_lines[i_y]
+        for i, j in product(range(grid_size[0]), range(grid_size[1])):
+            if symmetric:
+                pt = np.cross(
+                        np.r_[x_axis[:2], -(x_axis_limits[i_x][0] + j)],
+                        np.r_[y_axis[:2], -(y_axis_limits[i_y][0] + i)])
+            else:
+                pt = np.cross(
+                        np.r_[x_axis[:2], -(x_axis_limits[i_x][0] + 2*j + i%2)],
+                        np.r_[y_axis[:2], -(y_axis_limits[i_y][0] + i)])
+            pt = (pt / pt[2]).astype(int)
+            grid_ids[i, j] = np.argmin(np.linalg.norm(pts_rounded - pt, axis=1))
 
-
-
-
-    ## grow from center, to provide some robustness to lens distortion
-    #pts_ids = {(0, 0): seed}
-    #last_len = 0
-    #while last_len < len(pts_ids) < expected_num_pts:
-    #    last_len = len(pts_ids)
-
-    #    # check for points that can be added
-    #    for i, pt in enumerate(pts):
-    #        if i in pts_ids.values(): continue
-    #        for v in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-    #            neighbor = tuple(pts_rounded[i, :2] + v)
-    #            if neighbor not in pts_ids:
-    #                continue
-    #            j = pts_ids[neighbor]
-    #            # make sure the error isn't changing too quickly
-    #            if np.linalg.norm((pts[j, :] - pts_rounded[j, :]) - (pts[i, :] - pts_rounded[i, :])) < 1e-1:
-    #                pts_ids[tuple(pts_rounded[i, :2])] = i
-    #                break
-
-    return pts_ids
+    return grid_ids
 
 def ransac_homography(pts, grid_size, symmetric):
     """find + pattern to recover homography"""
@@ -173,83 +204,42 @@ def ransac_homography(pts, grid_size, symmetric):
         nearest_neighbors = np.argsort(dists)
         assert dists[nearest_neighbors[0]] < 1e-9, f"{nearest_neighbors}"
 
-        # TODO: try other combinations of points, checking if seed is at the intersection of p1-p2 and p3-p4
-        nplus = [nearest_neighbors[1], -1, -1, -1]
-        v1 = pts[nplus[0], :] - pt
-        for i in nearest_neighbors[2:]:
-            v2 = pts[i, :] - pt
-            if np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)) < -cos_thresh:
-                nplus[1] = i
-                break
-        if nplus[1] == -1: continue
+        for n_plus in combinations(nearest_neighbors[:10], 4):
+            n_pts = pts[n_plus, :]
+            intersection = np.cross(np.cross(n_pts[0], n_pts[1]), np.cross(n_pts[2], n_pts[3]))
+            intersection /= intersection[2]
+            if np.linalg.norm(intersection - pt) > 1e-2:
+                continue
 
-        for i in nearest_neighbors[2:]:
-            if i in nplus: continue
-            v3 = pts[i, :] - pt
-            if -cos_thresh < np.dot(v1, v3)/(np.linalg.norm(v1) * np.linalg.norm(v3)) < cos_thresh:
-                nplus[2] = i
-                break
-        if nplus[2] == -1: continue
+            h = estimate_homography(n_pts)
 
-        for i in nearest_neighbors[2:]:
-            if i in nplus: continue
-            v4 = pts[i, :] - pt
-            if np.dot(v3, v4)/(np.linalg.norm(v3) * np.linalg.norm(v4)) < -cos_thresh:
-                nplus[3] = i
-                break
-        if nplus[3] == -1: continue
+            pts_plane = pts @ h.T
+            pts_plane /= pts_plane[:, 2:]
 
-        h = estimate_homography(pts[nplus, :])
+            # first check: does this map the seed to the origin?
+            if np.linalg.norm(pts_plane[seed, :2]) > 1e-2: continue
 
-        pts_plane = pts @ h.T
-        pts_plane /= pts_plane[:, 2:]
+            grid_ids = find_grid_points(pts_plane, seed, grid_size, symmetric)
+            if grid_ids is None: continue
 
-        # first check: does this map the seed to the origin?
-        if np.linalg.norm(pts_plane[seed, :2]) > 1e-2: continue
-
-        grid_ids = find_grid_points(pts_plane, seed, grid_size, symmetric)
-        print(grid_ids)
-        exit()
-
-        inliers = list(pts_ids.values())
-        outliers = list(set(range(pts_plane.shape[0])) - set(inliers))
-
-        #grid_pts = []
-        #if symmetric:
-        #    if (max(idx_range[:, 1] - idx_range[:, 0]) == max(grid_size)-1 and
-        #        min(idx_range[:, 1] - idx_range[:, 0]) == min(grid_size-1)):
-        #        for i, j in product(range(idx_range[0, 1] - idx_range[0, 0]), range(idx_range[1, 1] - idx_range[1, 0])):
-        #            grid_pts.append(pts_ids[(i+idx_range[0,0], j+idx_range[1,0])])
-        #    else:
-        #        # handle diagonal shifted case
-        #        pass
-        #else:
-        #    if (max(idx_range[:, 1] - idx_range[:, 0]) == grid_size[0]//2 + grid_size[1] - 1 and
-        #        min(idx_range[:, 1] - idx_range[:, 0]) == (grid_size[0]+1)//2 + grid_size[1] - 2):
-        #        idx = 0
-        #        for i in range(idx_range[0, 0], idx_range[0, 1]+1):
-        #            pass
-
-
-
-        plt.subplot(1, 2, 1)
-        plt.plot(pts[:expected_num_pts, 0], pts[:expected_num_pts, 1], '.')
-        plt.plot(pts[expected_num_pts:, 0], pts[expected_num_pts:, 1], '.')
-        plt.plot(pts[nplus, 0], pts[nplus, 1], 'o')
-        plt.gca().set_aspect('equal')
-        plt.subplot(1, 2, 2)
-        plt.plot(pts_plane[inliers, 0], pts_plane[inliers, 1], '.')
-        plt.plot(pts_plane[outliers, 0], pts_plane[outliers, 1], '.')
-        #for b in bounds:
-        #    pt0 = -b[1][:2] * b[1][2]
-        #    pt1 = pt0 + 3 * b[1][:2] @ np.array([[0, 1], [-1, 0]])
-        #    pt2 = pt0 - 3 * b[1][:2] @ np.array([[0, 1], [-1, 0]])
-        #    plt.gca().annotate(str(b[0]), pt0, fontsize="x-small")
-        #    pts = np.stack((pt1, pt2))
-        #    plt.plot(pts[:, 0], pts[:, 1])
-        plt.gca().set_aspect('equal')
-        plt.show()
-        return grid_pts
+            plt.subplot(1, 2, 1)
+            plt.plot(pts[:expected_num_pts, 0], pts[:expected_num_pts, 1], '.')
+            plt.plot(pts[expected_num_pts:, 0], pts[expected_num_pts:, 1], '.')
+            plt.plot(pts[n_plus, 0], pts[n_plus, 1], 'o')
+            plt.plot(pts[grid_ids.flat, 0], pts[grid_ids.flat, 1], ':')
+            plt.gca().set_aspect('equal')
+            plt.subplot(1, 2, 2)
+            #plt.plot(pts_plane[list(inliers), 0], pts_plane[list(inliers), 1], '.')
+            #pp_rounded = np.rint(pts_plane)
+            #plt.plot(pp_rounded[list(inliers), 0], pp_rounded[list(inliers), 1], '.')
+            plt.plot(pts_plane[:, 0], pts_plane[:, 1], '.')
+            plt.plot(pts_plane[grid_ids.flat, 0], pts_plane[grid_ids.flat, 1], ':')
+            plt.gca().set_aspect('equal')
+            plt.gca().set_xticks(range(*map(int, plt.gca().get_xlim())))
+            plt.gca().set_yticks(range(*map(int, plt.gca().get_ylim())))
+            plt.gca().grid()
+            plt.show()
+            return grid_ids
 
 if __name__ == "__main__":
 
@@ -267,7 +257,7 @@ if __name__ == "__main__":
     K1 = -1e-2
     K2 = 5e-5
 
-    num_noise_pts = 2
+    num_noise_pts = 20
     noise_limit = 2
     ex_dist = 0.01
     sigma = 5e-5
@@ -288,7 +278,7 @@ if __name__ == "__main__":
                     ), H, K1, K2), sigma)
 
     ransac_homography(asymm_grid, asymm_grid_size, False)
-    exit()
+    ransac_homography(symm_grid, symm_grid_size, True)
     plt.subplot(1, 2, 1)
     plt.plot(symm_grid[:-num_noise_pts, 0], symm_grid[:-num_noise_pts, 1], 'o')
     plt.plot(symm_grid[-num_noise_pts:, 0], symm_grid[-num_noise_pts:, 1], 'o')
